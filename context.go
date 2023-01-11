199dc4b28b82f8994ea31bba81804e2cd82472f8
Harsh Gupta <guptaharsh9589@gmail.com>
2023-01-11 17:37:36 +0530

    second branch

--- context.go
+++ context.go
@@ -6,55 +6,53 @@ import (
 	"time"
 )
 
-
-type bid struct{
+type Bid struct {
 	adurl string
 	price float64
 }
 
-func bestBid(url string)bid{
-time.Sleep(20*time.Millisecond)
+func bestBid(url string) Bid {
+	time.Sleep(20 * time.Millisecond)
 
-return bid{
-adurl: "https://adsRus.com/12",
-price: 0.09,
+	return Bid{
+		adurl: "https://adsRus.com/12",
+		price: 0.09,
+	}
+
 }
 
-}
-
-var defaultBid=bid{
+var defaultBid = Bid{
 	adurl: "https://adsRus.com/default",
 	price: 0.02,
-	
 }
 
-func findBid(ctx context.Context,url string)bid{
-	ch:=make(chan bid ,1)//buffered to avoid goroutine leak
+func findBid(ctx context.Context, url string) Bid {
+	ch := make(chan Bid, 1) //buffered to avoid goroutine leak
 
-	go func(){
-		ch<-bestBid(url)
+	go func() {
+		ch <- bestBid(url)
 	}()
 
-	select{
-	case bid1:=<-ch:
-		return bid1
+	select {
+	case Bid1 := <-ch:
+		return Bid1
 	case <-ctx.Done():
 		return defaultBid
 	}
 }
 
-func main(){
-	ctx,cancel:=context.WithTimeout(context.Background(),100*time.Millisecond)
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
 	defer cancel()
-	url:="https://httpl.cat/23"
-	bid1:=findBid(ctx,url)
-	fmt.Println(bid1)
+	url := "https://httpl.cat/23"
+	Bid1 := findBid(ctx, url)
+	fmt.Println(Bid1)
 
 	fmt.Println("----------")
 
-	ctx,cancel=context.WithTimeout(context.Background(),10*time.Millisecond)
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Millisecond)
 	defer cancel()
-	url1:="https://httpl.cat/2387"
-	bid2:=findBid(ctx,url1)
-	fmt.Println(bid2)
+	url1 := "https://httpl.cat/2387"
+	Bid2 := findBid(ctx, url1)
+	fmt.Println(Bid2)
 }
