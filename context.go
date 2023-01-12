2075e8e65e700d725bec2bd65edde680232e76af
Harsh Gupta <guptaharsh9589@gmail.com>
2023-01-12 10:34:49 +0530

    b

--- context.go
+++ context.go
@@ -50,8 +50,8 @@ func main() {
 
 	fmt.Println("----------")
 
-	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Millisecond)
-	defer cancel()
+	ctx, cancll = context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancll()
 	url1 := "https://httpl.cat/2387"
 	Bid1:= findBid(ctx, url1)
 	fmt.Println(Bid1)
